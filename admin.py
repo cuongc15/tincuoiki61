c3c85e9088c227cd9e202a8139fb72b801dc4fe7
cuongc15 <69023803+cuongc15@users.noreply.github.com>
2026-03-05 15:18:25 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import re
 from supabase import create_client
-from supabase import create_client
-from dotenv import load_dotenv
-load_dotenv()
 import docx
 from PyPDF2 import PdfReader
 
